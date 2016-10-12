9255618687cddc9fbca5c52d8dff630f26a8cbba
nguyenle <nguyen.levu@smartdev.vn>
2016-10-12 17:57:24 +0700

    request

--- src/main/java/org/hocviencntt/controller/HomeController.java
+++ src/main/java/org/hocviencntt/controller/HomeController.java
@@ -1,8 +1,33 @@
 package org.hocviencntt.controller;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		System.out.println(" INFO Welcome home! The client locale is {}."+ locale);
+		//adding some time lag to check interceptor execution
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		System.out.println("INFO Before returning view page");
+		return "home";
+	}
 }
---
+++ src/main/java/org/hocviencntt/controller/RequestProcessingTimeInterceptor.java
@@ -0,0 +1,7 @@
+package org.hocviencntt.controller;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class RequestProcessingTimeInterceptor extends HandlerInterceptorAdapter {
+
+}
